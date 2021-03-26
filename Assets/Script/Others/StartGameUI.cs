2903f33b0ecb71251699382e88e6e77c9d57c3b2
hcthcm290 <dragonnica123@gmail.com>
2021-03-27 02:16:18 +0700

    Make the start button

---
+++ Assets/Script/Others/StartGameUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameUI : MonoBehaviour
+{
+    [SerializeField]
+    PlayerMovement playerMovement;
+    [SerializeField]
+    WaveSpawner waveSpawner;
+    [SerializeField]
+    GameObject playerCanon;
+    [SerializeField]
+    GameObject button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement.enabled = false;
+        waveSpawner.enabled = false;
+        playerCanon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        playerMovement.enabled = true;
+        playerCanon.SetActive(true);
+        button.SetActive(false);
+
+        waveSpawner.enabled = true;
+    }
+}
