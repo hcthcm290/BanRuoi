d9a43b7951073365f5d6fa4bd4a16e1405262d1f
hcthcm290 <dragonnica123@gmail.com>
2021-04-10 11:01:56 +0700

    Add score when defeat Enemy

---
+++ Assets/Script/Others/EnemyDeathScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathScore : MonoBehaviour
+{
+    [SerializeField] HealthBase health;
+    [SerializeField] float score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health.HPChange += OnHealthUpdate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnHealthUpdate(float currentHealth)
+    {
+        if(currentHealth <= 0)
+        {
+            PlayerPrefs.SetFloat("score", PlayerPrefs.GetFloat("score") + score);
+        }
+    }
+}
---
+++ Assets/Script/Others/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!PlayerPrefs.HasKey("score"))
+        {
+            PlayerPrefs.SetFloat("score", 0);
+        }
+        score.text = PlayerPrefs.GetFloat("score").ToString();
+    }
+}
